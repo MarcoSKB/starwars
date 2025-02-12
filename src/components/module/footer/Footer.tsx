9b7fe5564795dfbf9a43353469ce9dc2ad44c1a0
Marco <serikbaev03beka@gmail.com>
2025-02-12 21:53:16 +0500

    Add Footer section

--- src/components/module/footer/Footer.tsx
+++ src/components/module/footer/Footer.tsx
@@ -1,5 +1,47 @@
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer className='py-3 md:py-5'>
+      <div className='mx-auto max-w-[1140px] px-4'>
+        <div className='flex flex-col gap-3 md:gap-5'>
+          <div className='flex gap-5 self-center md:self-end'>
+            <a
+              href='https://www.facebook.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='transition-opacity hover:opacity-70'
+              title='To Facebook page'
+            >
+              <img src='./images/facebook.svg' alt='Facebook' />
+            </a>
+            <a
+              href='https://www.instagram.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='transition-opacity hover:opacity-70'
+              title='To Instagram page'
+            >
+              <img src='./images/instagram.svg' alt='Instagram' />
+            </a>
+            <a
+              href='https://x.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='transition-opacity hover:opacity-70'
+              title='To Twitter page'
+            >
+              <img src='./images/twitter.svg' alt='Twitter' />
+            </a>
+          </div>
+          <hr className='border-accent' />
+          <div className='mx-auto flex w-full max-w-[920px] flex-wrap justify-between gap-2 text-sm font-semibold'>
+            <span>© 2025 Star Wars</span>
+            <span>Privacy policy</span>
+            <span>Term of service</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
