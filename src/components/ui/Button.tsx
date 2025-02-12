8619ff9ca302494369a913343c5d91c2fc31dcf4
Marco <serikbaev03beka@gmail.com>
2025-02-12 21:52:22 +0500

    Add adaptive for button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,18 +1,26 @@
 interface Props {
   title: string
   onClick: () => void
+  isLoading?: boolean
 }
 
 const Button: React.FC<Props> = (props) => {
-  const { title, onClick } = props
+  const { title, onClick, isLoading = false } = props
 
   return (
     <button
       type='button'
-      className='text-primary text-md border-primary flex items-center justify-center border-2 border-solid px-6 py-4 leading-2 font-semibold transition-all hover:opacity-80 active:translate-y-1'
+      className='text-primary border-primary flex items-center justify-center gap-2 border-2 border-solid px-3 py-1 text-base font-semibold transition-all hover:cursor-pointer hover:opacity-80 active:translate-y-1 disabled:cursor-default disabled:opacity-50 disabled:active:translate-y-0 sm:gap-3 sm:px-4 sm:py-2'
       onClick={() => onClick()}
+      disabled={isLoading}
     >
-      <svg className='mr-3 size-5 animate-spin' viewBox='0 0 24 24' />
+      {isLoading && (
+        <img
+          className='h-6 w-6 animate-spin'
+          src='./images/tube-spinner.svg'
+          alt='Spinner icon'
+        />
+      )}
       {title}
     </button>
   )
