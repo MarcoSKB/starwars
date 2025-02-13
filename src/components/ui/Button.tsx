0db5e3a52f799f1487eaa357531690975f838766
Marco <serikbaev03beka@gmail.com>
2025-02-13 10:15:27 +0500

    Change button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,18 +1,19 @@
 interface Props {
   title: string
-  onClick: () => void
+  onClick?: () => void
   isLoading?: boolean
+  type?: 'button' | 'submit' | 'reset'
 }
 
 const Button: React.FC<Props> = (props) => {
-  const { title, onClick, isLoading = false } = props
+  const { title, onClick, isLoading = false, type = 'button' } = props
 
   return (
     <button
-      type='button'
-      className='text-primary border-primary flex items-center justify-center gap-2 border-2 border-solid px-3 py-1 text-base font-semibold transition-all hover:cursor-pointer hover:opacity-80 active:translate-y-1 disabled:cursor-default disabled:opacity-50 disabled:active:translate-y-0 sm:gap-3 sm:px-4 sm:py-2'
-      onClick={() => onClick()}
+      type={type}
+      className='text-primary border-primary active:border-accent flex w-full items-center justify-center gap-2 border-2 border-solid px-3 py-1 text-base font-semibold transition-all hover:cursor-pointer hover:opacity-80 active:translate-y-1 disabled:cursor-default disabled:opacity-50 disabled:active:translate-y-0 sm:gap-3 sm:px-4 sm:py-2'
       disabled={isLoading}
+      onClick={onClick}
     >
       {isLoading && (
         <img
