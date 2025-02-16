354efe5f19a04650e769ee0163a078832104887d
Marco <serikbaev03beka@gmail.com>
2025-02-16 10:54:04 +0500

    Change NotFound page style

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -2,7 +2,10 @@ import { Link } from '@components/ui'
 
 const NotFoundPage = () => {
   return (
-    <div className='flex h-full flex-col items-center justify-center'>
+    <div className='relative flex h-full flex-col items-center justify-center py-12'>
+      <p className='pointer-events-none absolute top-[46%] left-1/2 -translate-1/2 text-[160px] leading-0 font-bold opacity-10 sm:text-[200px] md:text-[300px]'>
+        404
+      </p>
       <p className='text-md'>Error 404</p>
       <h1 className='text-md mb-2 font-bold md:text-lg'>Page not found</h1>
       <Link to='/' replace>
