fb857a3c0e9a9db960e5a0eae9c77228b8b5dc65
Marco <serikbaev03beka@gmail.com>
2025-02-16 10:23:09 +0500

    Change link component in NotFound page

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,15 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Link } from '@components/ui'
 
 const NotFoundPage = () => {
   return (
     <div className='flex h-full flex-col items-center justify-center'>
       <p className='text-md'>Error 404</p>
       <h1 className='text-md mb-2 font-bold md:text-lg'>Page not found</h1>
-      <Link
-        to='/'
-        className='text-accent transition-transform hover:translate-x-3'
-        replace
-      >
+      <Link to='/' replace>
         → Go to home page
       </Link>
     </div>
