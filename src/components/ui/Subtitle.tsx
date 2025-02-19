6b78ad8b0afc98d4c28ca62eb86b62c8867c9cac
Marco <serikbaev03beka@gmail.com>
2025-02-19 20:14:20 +0500

    Add font-family to Subtitle ui component

--- src/components/ui/Subtitle.tsx
+++ src/components/ui/Subtitle.tsx
@@ -6,7 +6,7 @@ interface Props {
 const Subtitle: React.FC<Props> = ({ children, className }) => {
   return (
     <span
-      className={`sm:text-md md:text-subtitle before:bg-accent relative max-w-[746px] pl-5 text-base text-pretty before:absolute before:top-1/2 before:left-0 before:h-[75%] before:w-[3px] before:-translate-y-1/2 before:rounded-2xl before:content-[""] md:pl-9 ${className}`}
+      className={`sm:text-md md:text-subtitle before:bg-accent font-jost relative max-w-[746px] pl-5 text-base text-pretty before:absolute before:top-1/2 before:left-0 before:h-[75%] before:w-[3px] before:-translate-y-1/2 before:rounded-2xl before:content-[""] md:pl-9 ${className}`}
     >
       {children}
     </span>
