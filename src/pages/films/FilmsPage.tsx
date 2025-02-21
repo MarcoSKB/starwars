c5d4801974db5d03f070f6823a9200901f8b6124
Marco <serikbaev03beka@gmail.com>
2025-02-21 13:53:16 +0500

    Remove unnecessary component from FilmsPage

--- src/pages/films/FilmsPage.tsx
+++ src/pages/films/FilmsPage.tsx
@@ -1,11 +1,9 @@
-import { Outlet } from 'react-router-dom'
 import { FilmsSection } from './module'
 
 const FilmsPage = () => {
   return (
     <div>
       <FilmsSection />
-      <Outlet />
     </div>
   )
 }
